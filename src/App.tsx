29570b26f30ba341e1480653c9782d0259c0f700
origuri <dlcksrb7@naver.com>
2023-12-11 16:45:17 +0900

    2023.12.11 - styled-conponents theme + ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,19 @@
-import styled, { keyframes } from "styled-components";
+import { useState } from "react";
+import styled from "styled-components";
 
-const Wapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  /* light일 땐 흰색 dark일 땐 검정색이 들어감 */
-  background-color: ${(props) => props.theme.backgroundColor};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-// rotate 0에서 360도 도는 애니메이션
-const examAnimation = keyframes`
-0%{
-  transform: rotate(0deg);
-}
-50%{
-  transform: rotate(360deg);
-  border-radius: 50px;
-}
-100%{
-  transform: rotate(0deg);
-}
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
-const Title = styled.h1`
+const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
-  font-size: 50px;
-`;
-
-const Box = styled.div`
-  // 1s는 1초동안 한바퀴 linear는 같은 속도로 움직이는 것, infinite는 무한히 돌아가는 것.
-  animation: ${examAnimation} 1s ease-in-out infinite;
-  // span 태그만 타겟팅 해서 css 설정
-  ${Title} {
-    &:hover {
-      font-size: 100px;
-    }
-  }
 `;
 
 function App() {
   return (
-    <Wapper>
-      <Title>hello</Title>
-    </Wapper>
+    <Container>
+      <H1>hello</H1>
+    </Container>
   );
 }
 
---
+++ src/styled.d.ts
@@ -0,0 +1,12 @@
+// import original module declarations
+import "styled-components";
+
+// and extend them!
+// interface를 변경시킴
+declare module "styled-components" {
+  export interface DefaultTheme {
+    textColor: string;
+    bgColor: string;
+    btnColor: string;
+  }
+}
